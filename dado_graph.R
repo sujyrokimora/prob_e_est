06ecdecec7399b9df12640746491769e6176d884
Xavier Cruz <cruz.xavier8@gmail.com>
2023-03-28 17:54:26 +0100

    New graphs
    Signed-off-by: Xavier Cruz <cruz.xavier8@gmail.com>

--- dado_graph.R
+++ dado_graph.R
@@ -1,7 +1,12 @@
 # require lattice package
 set.seed(1)
-amostra <- sample(x=1:6,size=10000,replace = TRUE,prob = NULL)
+amostra <- sample(x = 1:6, size = 10000, replace = TRUE, prob = NULL)
 table(amostra)
-tabela<- table(amostra)
+tabela <- table(amostra)
 
-barplot(tabela,main = "freq absolt.",xlab = "Dado simulado",col = "red")
+barplot(tabela, main = "freq absolt.", xlab = "Dado simulado", col = "red", ylim = c(0, 2000))
+
+freq.rel <- tabela / length(amostra)
+barplot(freq.rel, main = "FREQ. REL.", xlab = "Dado Sim", col = "blue", ylim = c(0, 0.3))
+
+abline(h = 1 / 6, col = "PURPLE")
