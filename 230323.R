46d0d305a7accb27094531d6fe53ec9f9d62f1fb
Xavier Cruz <cruz.xavier8@gmail.com>
2023-03-28 17:30:09 +0100

    \	new file:   .idea/.gitignore 	new file:   .idea/.name 	new file:   .idea/discord.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/rGraphicsSettings.xml 	new file:   .idea/rSettings.xml 	new file:   .idea/vcs.xml 	new file:   20032023.R 	new file:   230323.R 	new file:   R_things.iml# 	new file:   main.R

---
+++ 230323.R
@@ -0,0 +1,2 @@
+set.seed(1)
+amostra <- sample(x=1:6,size=10000,replace = TRUE,prob = NULL)
---
+++ teste.R
@@ -0,0 +1,9 @@
+a<-2
+b<-1
+a+b
+meus.dados<-iris
+str(meus.dados)
+head(meus.dados)
+typeof(meus.dados)9
+minha.lista<- list(var1=matrix(1:12,ncol=3), var2="amanha")
+minha.lista
