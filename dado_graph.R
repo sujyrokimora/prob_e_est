39dc50758d0c8e4ebef32afae63807122771f39a
Xavier Cruz <cruz.xavier8@gmail.com>
2023-03-28 17:41:28 +0100

    modified:   dado_graph.R

--- dado_graph.R
+++ dado_graph.R
@@ -3,4 +3,5 @@ set.seed(1)
 amostra <- sample(x=1:6,size=10000,replace = TRUE,prob = NULL)
 table(amostra)
 tabela<- table(amostra)
-barplot(tabela)
+
+barplot(tabela,main = "freq absolt.",xlab = "Dado simulado",col = "red")
